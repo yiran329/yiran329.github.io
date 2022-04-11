d076a369ac4b3a48f50a228429bdbe4398cff52f
王怡然 <2374454883@qq.com>
2022-04-11 22:50:45 +0800

    0411

--- puzzle2_script.js
+++ puzzle2_script.js
@@ -1,8 +1,18 @@
-var answers = ["野狼disco","2"]
+var answers = ["野狼disco","雕虫小技"]
 
-function enterAnswer(){
-  var response = prompt("Please enter your answer");
+function enterAnswer1(){
+  var response = prompt("Please enter your answer :)");
   if (response === answers[0]){
+    alert("You're right!");
+  }
+  else{
+    alert("Sorry, you're wrong. Please think again.");
+  }
+}
+
+function enterAnswer2(){
+  var response = prompt("Please enter your answer :)");
+  if (response === answers[1]){
     alert("You're right!");
   }
   else{
