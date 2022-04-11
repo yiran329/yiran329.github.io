130b34008c1f4abc6e75e75480b73f39544079f5
王怡然 <2374454883@qq.com>
2022-04-11 19:47:25 +0800

    puzzle

---
+++ puzzle_script.js
@@ -0,0 +1,9 @@
+function pressbutton(){
+  var x = document.getElementById('demo');
+  x.style.fontSize = "25px";
+  x.style.color = "deeppink";
+  x.innerHTML = "HAHA,you have no choice~ *^v^*";
+}
+
+var Name = prompt("What's your name?")
+alert("Hello, "+ Name +", welcome to this page! ^o^")
