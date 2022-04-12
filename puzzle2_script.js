1c95f2285f583b642c69619395b5f89b603b4c47
王怡然 <2374454883@qq.com>
2022-04-12 15:52:03 +0800

    0412

--- puzzle2_script.js
+++ puzzle2_script.js
@@ -1,4 +1,4 @@
-var answers = ["野狼disco","雕虫小技"]
+var answers = ["野狼disco","心有余悸","孙红雷","千变万化","格格不入"]
 
 function enterAnswer1(){
   var response = prompt("Please enter your answer :)");
@@ -13,6 +13,36 @@ function enterAnswer1(){
 function enterAnswer2(){
   var response = prompt("Please enter your answer :)");
   if (response === answers[1]){
+    alert("You're right!");
+  }
+  else{
+    alert("Sorry, you're wrong. Please think again.");
+  }
+}
+
+function enterAnswer3(){
+  var response = prompt("Please enter your answer :)");
+  if (response === answers[2]){
+    alert("You're right!");
+  }
+  else{
+    alert("Sorry, you're wrong. Please think again.");
+  }
+}
+
+function enterAnswer4(){
+  var response = prompt("Please enter your answer :)");
+  if (response === answers[3]){
+    alert("You're right!");
+  }
+  else{
+    alert("Sorry, you're wrong. Please think again.");
+  }
+}
+
+function enterAnswer5(){
+  var response = prompt("Please enter your answer :)");
+  if (response === answers[4]){
     alert("You're right!");
   }
   else{
